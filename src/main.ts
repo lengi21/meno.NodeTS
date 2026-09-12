8490529a2ffd6fe8de8843d93bbb45aeb56cf6a8
10go <mixelidzet@gmail.com>
2026-09-12 11:26:59 +0400

    unversuiio ned

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module.js';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableCors({
+    origin: process.env.APP_ORIGIN?.split(',') ?? ['http://localhost:4200'],
+    credentials: true,
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  await app.listen(Number(process.env.PORT ?? 3001));
+}
+await bootstrap();
